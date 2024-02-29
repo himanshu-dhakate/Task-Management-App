7848a03b52661516e51a005a86c0d1fe4c474303
himanshu-dhakate <dhakate.himanshu9@gmail.com>
2024-02-29 19:44:23 +0530

    Update Delete Functionality

--- todo_project/trelloapp/urls.py
+++ todo_project/trelloapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('tasklists/', views.index, name = "index"),
     path('addnewtasklist/', views.create_tasklist, name = "addtasklist"),
     path('addnewtask/<int:id>', views.create_task_in_tasklist, name = "addtasks"),
-    path('showtasks/<int:id>', views.showTasks, name = "mytasks"),
-    path('checkboxonoff/', views.check_uncheck_checkbox, name = "check_uncheck")
+    path('showtasks/<int:id>/<str:listname>/', views.showTasks, name = "mytasks"),
+    path('checkboxonoff/', views.check_uncheck_checkbox, name = "check_uncheck"),
+    path("updatetask/<int:id>", views.update_task, name = "updatetask"),
+    path("deletetask/<int:id>", views.delete_task, name = "deletetask"),
+    path("deletelist/<int:id>", views.delete_list, name = "deletelist")
 ]
